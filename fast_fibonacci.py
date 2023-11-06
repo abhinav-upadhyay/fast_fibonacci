02f28b20f3e0aaf15c2bd7928842c64561b526bf
Abhinav Upadhyay <er.abhinav.upadhyay@gmail.com>
2023-11-07 02:52:51 +0530

    Implement a native Python implementation of the M matrix So as to avoid overflow possiblities with numpy-128 bit float.
    The golden ratio based formula still uses them because it is a
    floating point value and there is no way getting around it.
    But it seems at least for n=5000, 128-bit float will work.

--- fast_fibonacci.py
+++ fast_fibonacci.py
@@ -1,24 +1,57 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-M = np.array([[1,1], [1,0]], dtype=np.float128)
+# Using 128 bit floats to handle largish values.
+# It seems at least for benchmarking upto first 5000 Fibonacci
+# numbers, 128-bit float works
 TAO_1 = np.float128((1 + np.sqrt(5)) / 2)
 TAO_2 = np.float128((1 - np.sqrt(5)) / 2)
 INV_SQRT_FIVE =  1 / np.sqrt(5)
 
+"""
+A hacky python implementation of the matrix M
+Python has arbitrary precision integers which makes it
+easier to compute large values without overflowing.
+"""
+class M:
+    def __init__(self, values=None):
+        if not values:
+            self.values = [[1, 1], [1, 0]]
+        else:
+            self.values = values
 
-def _comput_M_n(n: int) -> np.ndarray:
-    return np.linalg.matrix_power(M, n)
 
+    def exp(m, n: int):
+        z = result = None
+        while n > 0:
+            z = m if z is None else z.mult(z) 
+            n, bit = divmod(n, 2)
+            if bit:
+                result = z if result is None else z.mult(result)
+        return result
+
+    def mult(self, x):
+        values = [[0, 0], [0, 0]]
+        values[0][0] = self.values[0][0] * x.values[0][0] + self.values[0][1] * x.values[1][0]
+        values[0][1] = self.values[0][0] * x.values[1][0] + self.values[0][1] * x.values[1][1]
+
+        values[1][0] = self.values[1][0] * x.values[0][0] + self.values[1][1] * x.values[1][0]
+        values[1][1] = self.values[1][0] * x.values[1][0] + self.values[1][1] * x.values[1][1]
+        return M(values)
+    
+    def __repr__(self):
+        return f"M([[{self.values[0][0]}, {self.values[0][1]}], [{self.values[1][0]}, {self.values[1][1]}]])"
+    
 
 def fib_fast(n: int) -> int:
-    M_n = _comput_M_n(n)
-    result = np.matmul(M_n, np.array([1, 0]))
+    M_n: M = M().exp(n)
+    result = []
+    result.append(M_n.values[0][0] * 1 + M_n.values[0][1] * 0)
+    result.append(M_n.values[1][0] * 1 + M_n.values[1][1] * 0)
     return result[1]
 
 def fib_closed_form(n: int) -> int:
     return INV_SQRT_FIVE * (np.power(TAO_1, n) - np.power(TAO_2, n))
-
 
 def fib(n: int) -> int:
     if n == 0:
@@ -39,7 +72,7 @@ if __name__ == '__main__':
     # collect timings for fib and fib_fast for n going from 2 to 5000 in increments of 10
     import time
     n_max = 5000
-    n_inc = 50
+    n_inc = 10
     fib_timings = {}
     fib_fast_timings = {}
     fib_closed_form_timings = {}
@@ -50,12 +83,13 @@ if __name__ == '__main__':
         fib_timings[n] = end - start
 
 
-
     for n in range(2, n_max+1, n_inc):
         start = time.time()
         fib_fast_res = fib_fast(n) 
         end = time.time()
         fib_fast_timings[n] = end - start
+        # fib_closed_res = fib_closed_form(n)
+        # assert np.isclose(np.float128(fib_fast_res), fib_closed_res)
 
 
     for n in range(2, n_max+1, n_inc):
@@ -73,12 +107,14 @@ if __name__ == '__main__':
     fib_fast_timings_values = list(fib_fast_timings.values())
     fib_closed_form_timings_values = list(fib_closed_form_timings.values())
 
+
     # Create the plot
     plt.figure(figsize=(10, 6))
     plt.plot(n_values, fib_timings_values, label='fib(n) Timings', marker='o', linestyle='-', color='blue')
     plt.plot(n_values, fib_fast_timings_values, label='fib_fast(n) Timings', marker='o', linestyle='-', color='green')
     plt.plot(n_values, fib_closed_form_timings_values, label='fib_closed_form(n) Timings', marker='o', linestyle='-', color='red')
 
+    # plt.yscale('log')
     # Label the axes and add a legend
     plt.xlabel('n')
     plt.ylabel('Execution Time (seconds)')
